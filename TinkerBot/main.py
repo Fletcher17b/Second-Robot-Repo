a583deb1ed483a4af22bda840613697dc3d61818
MGRILLO - Fletcher <96928501+Fletcher17b@users.noreply.github.com>
2024-05-27 21:29:41 -0600

    Update to May 27 5:00pm
    Drake is overrated

--- TinkerBot/main.py
+++ TinkerBot/main.py
@@ -17,11 +17,11 @@ crane_motor = Motor(Port.B)
 claw_motor = Motor(Port.C)
 
 claw_sensor = ColorSensor(Port.S4)
-
+gyro_sensor = GyroSensor(Port.S2)
 LineSensor_left = ColorSensor(Port.S1)
 LineSensor_right = ColorSensor(Port.S3)
 
-robot = DriveBase(green_motor,blue_motor,34,150)
+robot = DriveBase(green_motor,blue_motor,68.8,185)
 
 #########
 #funciones'
@@ -37,26 +37,43 @@ def turn_left(angle):
     robot.curve(120,angle,then=Stop, wait=False)
 
 def unstuck():
-    crane_motor.run_until_stalled(100, then=Stop.HOLD, duty_limit=41)  
+    crane_motor.run_until_stalled(100, then=Stop.HOLD, duty_limit=41) 
+    crane_motor.run_time(speed=90, time=200)
 
+    
+ 
+def initial_claw_position():
+    g=1;
 
 def grab():
 #
     claw_motor.run_until_stalled(100)
     wait(1000)
-    crane_motor.run_time(speed=100, time=1000, then=Stop.HOLD, wait=True)
-    print("done")
+    crane_motor.run_time(speed=100, time=500, then=Stop.HOLD, wait=True)
     
+def drop_ontop():
+    print("1")
+    claw_motor.run_until_stalled(100, then=Stop.HOLD, duty_limit=41)
+ #   crane_motor.run_angle(speed=100, rotation_angle=-40)
+    crane_motor.stalled()
+    print("2")
+    robot.drive(speed=94,turn_rate=0)
+    wait(6000)
+    robot.stop()
+    print("3")
+    
+    drop()
 
 def drop():
-    claw_motor.run_until_stalled(100, then=Stop.HOLD, duty_limit=41)
-    crane_motor.run_angle(speed=100, rotation_angle=-40)
+    claw_motor.run_angle(speed=100, rotation_angle=-90, wait=True)
+ #   crane_motor.run_angle(speed=100, rotation_angle=-40)
     crane_motor.stalled()
+
 #    claw_motor.run_angle(speed=100, rotation_angle=40, wait=False)
 #    claw_motor.run_angle(speed=100, rotation_angle=40, then=Stop.HOLD, wait=False)
 #    crane_motor.run_angle(speed=1000, rotation_angle=1500, then=Stop.HOLD, wait=True)
 
-print("done")
+
 # claw angle range is 45 degrees to open and close 
 
 
@@ -75,14 +92,51 @@ def initial_moveset(start_type):
 
 try:
     # claw_motor.run_angle(speed=100, rotation_angle=90, wait=True)
-    # wait(1000)
     # drop()
     # wait(1000)
     unstuck()
+    robot.drive(speed=-94,turn_rate=0)
     wait(1000)
-    claw_motor.run_angle(speed=100, rotation_angle=-60, wait=True)
+    robot.stop()
+    wait(1000)
+    robot.drive(speed=94,turn_rate=0)
+    wait(1700)
+    robot.stop()
+    wait(100)
+    green_motor.run_time(speed=90,time=2200,wait=False)
+    blue_motor.run_time(speed=-90,time=3000,wait=True)
+    print("advance")
+
+
+    robot.drive(speed=94,turn_rate=0)
+    wait(3000)
+    print("a")
+    robot.stop()
+    print("a")
+    green_motor.run_time(speed=90,time=2200,wait=False)
+    blue_motor.run_time(speed=-90,time=3000,wait=True)
+
+    drop()
+    robot.drive(speed=94,turn_rate=0)
+    wait(1750)
+    print("a")
+    robot.stop()
+    print("a")
     wait(1000)
     grab() 
+
+#    unstuck()
+#    print("Unstuck exits")
+#    wait(1000)
+#    claw_motor.run_angle(speed=100, rotation_angle=-40, wait=True)
+#    wait(1000)
+#    
+#    grab() 
+#    print("Grab exits")
+    
+#    drop_ontop()
+#    print("Drop EXITS")
+
     # wait(2000)
     # initial_moveset(1)
 
