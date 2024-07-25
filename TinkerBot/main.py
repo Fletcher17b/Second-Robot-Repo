366cb7a9445fcf8730edd43699b7a144c792b093
DaniHB // LemonpieGBS <142750834+LemonpieGBS@users.noreply.github.com>
2024-07-25 12:30:49 -0600

    agregar algo muy MUY importante

--- TinkerBot/main.py
+++ TinkerBot/main.py
@@ -4,6 +4,8 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port    
 from pybricks.tools import wait
 from pybricks.hubs import EV3Brick
+
+import threading
 #Esto no fue posible de modularizar, por lo que se tuvo que hacer en el main
 #Sin embargo, el hecho de que las funciones no se puedan modularizar, hace que entonces
 #sea más claro cuando hay reset.angle o reset
@@ -15,66 +17,130 @@ import funcionesDesplazamiento as fd
 import funcionesGarraYElevador as ge
 import definitions as df
 
+def play_song():
+    while(True): 
+        df.ev3.speaker.beep(500, 214)
+        wait(214)
+
+        df.ev3.speaker.beep(500, 214)
+        df.ev3.speaker.beep(420, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(500, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(500, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(472, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(375, 214)
+
+        df.ev3.speaker.beep(749, 214)
+
+        wait(107)
+        df.ev3.speaker.beep(749, 214)
+
+        wait(107)
+        df.ev3.speaker.beep(630, 214)
+
+        df.ev3.speaker.beep(500, 214)
+        wait(214)
+
+        df.ev3.speaker.beep(500, 214)
+        df.ev3.speaker.beep(420, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(500, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(500, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(472, 214)
+
+        wait(214)
+        df.ev3.speaker.beep(375, 214)
+
+        df.ev3.speaker.beep(667, 214)
+
+        wait(107)
+        df.ev3.speaker.beep(667, 214)
+
+        wait(107)
+        df.ev3.speaker.beep(630, 214)
+
 # Description: Trayectoria de TinkerBot
 
-ge.cerrar_hasta_top()
+def main(): 
 
-#para atras acomodarse 1 
-fd.movimientoRecto(-200)
+    ge.cerrar_hasta_top()
 
-#df.robot.reset()
+    #para atras acomodarse 1 
+    fd.movimientoRecto(-200)
 
-#para adelante acomodarse 1
+    #df.robot.reset()
 
-ge.abrir_garra()
+    #para adelante acomodarse 1
 
-fd.movimientoRecto(335)
+    ge.abrir_garra()
 
-ge.cerrar_hasta_top()
+    fd.movimientoRecto(335)
 
-ge.moverElevadorGrua(True,90)
+    ge.cerrar_hasta_top()
 
-fd.movimientoRecto(125)
+    ge.moverElevadorGrua(True,90)
 
-df.ev3.speaker.beep()
-wait(500)
-df.robot.stop()
-e1.reset_angle(0)
-e2.reset_angle(0)
+    fd.movimientoRecto(125)
 
-df.robot.reset()
+    wait(500)
+    df.robot.stop()
+    e1.reset_angle(0)
+    e2.reset_angle(0)
 
-#aqui iria un cerrar
+    df.robot.reset()
 
-fd.girar(91)
+    #aqui iria un cerrar
 
-fd.movimientoRecto(420)
+    fd.girar(91)
 
-df.ev3.speaker.beep()
-wait(500)
-ge.moverElevadorGrua(True,240)
-fd.girar(-30)
+    fd.movimientoRecto(410)
 
-fd.girar(30)
+    wait(500)
+    ge.moverElevadorGrua(True,240)
+    fd.girar(-30)
 
-df.ev3.speaker.beep()
-wait(500)
-fd.movimientoRecto(-10)
+    fd.girar(30)
 
-fd.girar(86)
 
-wait(200)
-fd.movimientoRecto(90)
+    fd.movimientoRecto(-10)
 
-ge.moverElevadorGrua(False,180)
-ge.abrir_garra()
+    fd.girar(86)
 
-wait(500)
-ge.moverElevadorGrua(False,140)
-fd.movimientoRecto(10)
-ge.moverElevadorGrua(True,70)
-ge.cerrar_hasta_top()
+    fd.movimientoRecto(90)
 
-ge.moverElevadorGrua(True,90)
-fd.girar(90)
-fd.movimientoRecto(1000)
+    ge.moverElevadorGrua(False,180)
+    ge.abrir_garra()
+    wait(500)
+
+    ge.moverElevadorGrua(False,140)
+    fd.movimientoRecto(10)
+    ge.moverElevadorGrua(True,70)
+    ge.cerrar_hasta_top()
+
+    ge.moverElevadorGrua(True,90)
+
+    fd.movimientoRecto(-60)
+    fd.girar(-89)
+
+    fd.movimientoRecto(-20000)
+    fd.girar(-89)
+
+    fd.movimientoRecto(200)
+
+thread1 = threading.Thread(target=play_song)
+thread2 = threading.Thread(target=main)
+
+thread1.start()
+thread2.start()
